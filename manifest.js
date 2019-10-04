22494c9f4db1cb7bd88884ab3bd6be89242fd0f5
Sleeyax <yourd3veloper@gmail.com>
2019-10-04 23:06:02 +0200

    Create project

---
+++ manifest.js
@@ -0,0 +1,12 @@
+const {description} = require('./package');
+
+module.exports = {
+    id: 'com.sleeyax.stremio-discord',
+    name: 'Discord rich presence',
+    description,
+    catalogs: [],
+    version: '0.0.1',
+    logo: 'https://i.imgur.com/HGIkQgD.png',
+    resources: ['subtitles'],
+    types: ['movie', 'series'],
+};
---
+++ server.js
@@ -0,0 +1,4 @@
+const {serveHTTP} = require('stremio-addon-sdk');
+const addonInterface = require('./addon');
+
+serveHTTP(addonInterface, {port: 7000});
