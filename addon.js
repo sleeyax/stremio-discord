cc9896dadfc44d8a3ba013bbc7dc7841c5054176
Sleeyax <yourd3veloper@gmail.com>
2019-10-04 23:50:55 +0200

    Create addon

---
+++ addon.js
@@ -0,0 +1,41 @@
+const manifest = require('./manifest');
+const {addonBuilder} = require('stremio-addon-sdk');
+const imdb = require('./imdb-promise');
+const updatePresence = require('./rpc');
+const parseMovieDuration = require('./movie-duration');
+
+const addon = new addonBuilder(manifest);
+
+function showStartupPresence() {
+    updatePresence({
+        largeImageKey: 'stremio-logo',
+        details: 'https://stremio.com',
+        state: 'All the video content you enjoy in one place',
+    });
+}
+
+showStartupPresence();
+
+addon.defineSubtitlesHandler(async args => {
+    const id = args.id.split(':')[0];
+    const info = await imdb(id);
+
+    if (info == null) throw 'stremio-discord: no imdb data found for ' + id;
+
+    updatePresence({
+        state: `⭐ ${info.rating}/10`,
+        details: `📺 ${info.title} (${info.year})`,
+        startTimestamp: Date.now(),
+        endTimestamp: parseMovieDuration(info.runtime),
+        largeImageKey: 'stremio-logo',
+    });
+
+    return Promise.resolve({subtitles: []});
+});
+
+addon.defineMetaHandler(() => {
+    showStartupPresence();
+    return Promise.resolve({meta: {}});
+});
+
+module.exports = addon.getInterface();
