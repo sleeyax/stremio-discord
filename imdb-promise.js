4811f866cec178d50aba198605898d37f6c3c4fc
sungshon <49482512+sungshon@users.noreply.github.com>
2019-10-07 15:52:01 +0300

    Use cinemeta instead of imdb module

--- imdb-promise.js
+++ imdb-promise.js
@@ -1,12 +1,19 @@
-const imdbApi = require('imdb');
+const needle = require('needle');
 
-function imdb(tt) {
+function imdb(args) {
     return new Promise((resolve, reject) => {
-        imdbApi(tt, function(err, data) {
-            if(err) reject(err);
-            if (!data) resolve(null);
-            resolve(data);
-        });
+    	let id = args.id
+    	if (!id.startsWith('tt'))
+    		return reject('Not an imdb id')
+    	if (id.includes(':'))
+    		id = id.split(':')[0]
+		needle.get('https://v3-cinemeta.strem.io/meta/' + args.type + '/' + id + '.json', (err, resp, body) => {
+			if ((body || {}).meta) {
+				resolve(body.meta)
+			} else {
+				reject(err || Error('Unknown error in cinemeta for request: ' + JSON.stringify(args)))
+			}
+		})
     });
 }
 
